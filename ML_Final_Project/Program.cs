bd6613019692e83401daf36c454df44db063c88d
Raidel Valdes <raidel2808@gmail.com>
2022-12-11 14:45:03 -0500

    Switch ok, case2 ok(Se imprime bien los puntos del cluster

--- ML_Final_Project/Program.cs
+++ ML_Final_Project/Program.cs
@@ -37,7 +37,8 @@ namespace ML_Final_Project
 
                 List<List<IrisData>> clusters = DBSCAN.GetClusters(list, eps, minPts);
 
-                Console.Clear();
+                Console.ForegroundColor=ConsoleColor.Green;
+                
 
                 
                 string op = "0"; //creando variable de seleccion del switch
@@ -54,56 +55,57 @@ namespace ML_Final_Project
                     switch (op)
                     {
                         case "1": // print points to console
-                                  Console.WriteLine("The {0} points are :\n", list.Count);
-                                  foreach (IrisData p in list) Console.Write(" {0} \n", p);
-                                  Console.WriteLine();
-                                  break;
-                       
-                        case "4":break;
-                        
-                        default: Console.WriteLine("Opcion incorrecta");
-                                 break;
+                            Console.WriteLine("The {0} data are :\n", list.Count);
+                            foreach (IrisData p in list) Console.Write(" {0} \n", p);
+                            Console.WriteLine();
+                            Console.WriteLine("Presione una tecla para continuar");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
+
+                        case "2": // print clusters to console
+                            int total = 0;
+                            for (int i = 0; i < clusters.Count; i++)
+                            {
+                                int count = clusters[i].Count;
+                                total += count;
+                                string plural = (count != 1) ? "s" : "";
+                                Console.WriteLine($"\nCluster {i + 1} consists of the following {count} point{plural} :\n");
+                                foreach (IrisData p in clusters[i]) Console.Write($" {p} \n");
+                                Console.WriteLine();
+                            }
+
+                            // print any points which are NOISE
+                            total = list.Count - total;
+                            if (total > 0)
+                            {
+                                string plural = (total != 1) ? "s" : "";
+                                string verb = (total != 1) ? "are" : "is";
+                                Console.WriteLine("\nThe following {0} point{1} {2} NOISE :\n", total, plural, verb);
+                                foreach (IrisData p in list)
+                                {
+                                    if (p.ClusterId == IrisData.NOISE) Console.Write(" {0} \n", p);
+                                }
+                                Console.WriteLine();
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nNo points are NOISE");
+                            }
+                            Console.WriteLine("Presione una tecla para continuar");
+                            Console.ReadLine();
+                            Console.Clear();
+                            break;
 
 
+                        case "4": break;
 
+                        default:
+                            Console.WriteLine("Opcion incorrecta");
+                            break;
 
                     }
                 }
-
-                // print clusters to console
-                int total = 0;
-                for (int i = 0; i < clusters.Count; i++)
-                {
-                    int count = clusters[i].Count;
-                    total += count;
-                    string plural = (count != 1) ? "s" : "";
-                    Console.WriteLine($"\nCluster {i+1} consists of the following {count} point{plural} :\n");
-                    foreach (IrisData p in clusters[i]) Console.Write($" {p} \n");
-                    Console.WriteLine();
-                }
-
-                // print any points which are NOISE
-                total = list.Count - total;
-                if (total > 0)
-                {
-                    string plural = (total != 1) ? "s" : "";
-                    string verb = (total != 1) ? "are" : "is";
-                    Console.WriteLine("\nThe following {0} point{1} {2} NOISE :\n", total, plural, verb);
-                    foreach (IrisData p in list)
-                    {
-                        if (p.ClusterId == IrisData.NOISE) Console.Write(" {0} ", p);
-                    }
-                    Console.WriteLine();
-                }
-
-                else
-                {
-                    Console.WriteLine("\nNo points are NOISE");
-                }
-                Console.WriteLine("Presione una tecla para continuar");
-                Console.ReadKey();
-                
-
             }
             catch (Exception ex)
             { 
